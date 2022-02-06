6cd1bc570387afd33bfd5ed65f30a99cb3296650
NMINHDAI <ngominhdai92tn@gmail.com>
2022-02-06 09:55:04 +0700

    2nd commit

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
-
-
+const winston = require('winston');
 const app = express();
 require('dotenv').config();
+const booksRoute = require('./routes/books');
+
+
 const PORT = process.env.PORT || 3000;
+// middle ware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+//create a logger
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+      new winston.transports.Console({
+        format:winston.format.combine(
+          winston.format.colorize({all:true})
+        )
+      }),
+      new winston.transports.File({ filename: 'error.log' ,level:'error'})
+    ],
+    exceptionHandlers: [
+      new winston.transports.File({ filename: 'exceptions.log' })
+    ]
+  });
+  
+
+//routes
+app.use('/api/books',booksRoute);
+
 //connect to mongo db
 mongoose
     .connect(
@@ -18,7 +44,7 @@ mongoose
     console.log('something happened',error);
 })
 
-
+//start the server
 app.listen(PORT, () => {
-    console.log('Server start at PORT', PORT);
-})
+    logger.info(`Server started at PORT ${PORT}`);
+  });
